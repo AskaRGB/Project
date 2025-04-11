e55f353732af214e11b987f1aa5007b5cd8f1f53
Askar <gabidullinaskar@gmial.com>
2025-04-11 13:41:33 +0300

    добавлена новая переменная в функцию sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -6,6 +6,6 @@ def filter_by_state(list_of_dictionaries: [list], state: str = "EXECUTED") -> Li
     return [item for item in list_of_dictionaries if item.get("state") == state]
 
 
-def sort_by_date(list_of_dictionaries: list) -> List[Dict]:
+def sort_by_date(list_of_dictionaries: list, sorting_direction: bool = True) -> List[Dict]:
     """Функция сортировки даты"""
-    return sorted(list_of_dictionaries, key=lambda item: item.get("state"), reverse=True)
+    return sorted(list_of_dictionaries, key=lambda item: item.get("state"), reverse=sorting_direction)
